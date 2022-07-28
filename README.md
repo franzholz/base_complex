9ae8f29d172c4dbca7bcd536b220e70ad59d437f
Franz Holzinger <franz.holzinger@festwein.de>
2022-07-28 19:09:51 +0200

    * initial creation based on PHPComplex v3.0.1

---
+++ README.md
@@ -0,0 +1,6 @@
+base_complex
+============
+This provides the PHPComplex number library.
+It is required for older versions of base_excel.
+
+
---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "base_svg".
+########################################################################
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Complex Numbers Library',
+    'description' => 'A library to deal with complex numbers. (PHPComplex)',
+    'category' => 'misc',
+    'version' => '0.0.1',
+    'state' => 'stable',
+    'clearcacheonload' => 1,
+    'author' => 'Fabien Ménager, Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'author_company' => 'jambage.com',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.4.0-8.1.99',
+            'typo3' => '9.5.0-11.5.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
